c30e5197092c2c52da7ab96946de6cf947c72e9f
Fresh <ssw8227@gmail.com>
2025-04-20 15:40:50 +0900

    2nd

--- src/acess/a/AccessMain.java
+++ src/acess/a/AccessMain.java
@@ -6,7 +6,7 @@ public class AccessMain {
         acessData.publicField = 12;
         acessData.publicMethod();
 
-        acessData.defaultField = 5;
+        acessData.defaultField = 10;
         acessData.defaultMethod();
 
         acessData.innerAccess();

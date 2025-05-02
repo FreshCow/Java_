26a55fcb41c0b3f060f147af37009402b810ba4c
Fresh <ssw8227@gmail.com>
2025-05-02 23:04:52 +0900

    상속화

---
+++ src/extends1/ex2/Carmain.java
@@ -0,0 +1,13 @@
+package extends1.ex2;
+
+public class Carmain {
+    public static void main(String[] args) {
+        ElectricCar electricCar = new ElectricCar();
+        electricCar.move();
+        electricCar.charge();
+
+        GasCar gasCar = new GasCar();
+        gasCar.move();
+        gasCar.fillUp();
+    }
+}
